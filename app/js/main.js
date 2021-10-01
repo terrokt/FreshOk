be18e58357397e7d4c9a396d848758c274e27482
terrokt <terrokt@outlook.com>
2021-10-01 21:46:50 +0300

    Сверстал до конца

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,38 @@
 $(function () {
-  $('.hero__slider').slick({
+  $('.hero__baner').slick({
     prevArrow: $('.hero__arrow-prev'),
     nextArrow: $('.hero__arrow-next'),
-    autoplay: true, 
+    autoplay: true,
   });
-  var mixer = mixitup('.products__list');
+
+  $('.header__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu--active');
+    $('.header__btn').toggleClass('header__btn--active');
+  });
+
+  
+  $(document).mouseup(function (e){ // событие клика по веб-документу
+    var div = $(".menu__list"); // Элемент, клик по которому не должен приводить к закрытию. 
+    if (!div.is(e.target) // если клик был не по нашему блоку
+        && div.has(e.target).length === 0) { // и не по его дочерним элементам
+       $('.menu__list').removeClass('menu--active');
+       $('.header__btn').removeClass('header__btn--active');
+    }
+  });
+
+  
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+
 });
